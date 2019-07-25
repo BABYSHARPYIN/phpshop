78c1b31f5fd7d9cf3701a55b163c6175dfebe084
babysharpyin <34263845+BABYSHARPYIN@users.noreply.github.com>
2019-07-25 19:17:50 +0800

    修改界面有一个小BUG，type_id没有穿入

--- db.sql
+++ db.sql
@@ -24,7 +24,7 @@ create table shop_goods
 	key addtime(addtime),
 	key brand_id(brand_id),
 	key is_on_sale(is_on_sale)
-)engine=InnoDB default charset=utf8 comment '商品';
+)engine=MyISAM default charset=utf8 comment '商品';
 
 drop table if exists shop_brand;
 create table shop_brand
@@ -34,7 +34,7 @@ create table shop_brand
 	site_url varchar(150) not null default '' comment '官方网址',
 	logo varchar(150) not null default '' comment '品牌Logo图片',
 	primary key (id)
-)engine=InnoDB default charset=utf8 comment '品牌';
+)engine=MyISAM default charset=utf8 comment '品牌';
 
 drop table if exists shop_member_level;
 create table shop_member_level
@@ -44,7 +44,7 @@ create table shop_member_level
 	jifen_bottom mediumint unsigned not null comment '积分下限',
 	jifen_top mediumint unsigned not null comment '积分上限',
 	primary key (id)
-)engine=InnoDB default charset=utf8 comment '会员级别';
+)engine=MyISAM default charset=utf8 comment '会员级别';
 
 drop table if exists shop_member_price;
 create table shop_member_price
@@ -54,7 +54,7 @@ create table shop_member_price
 	goods_id mediumint unsigned not null comment '商品Id',
 	key level_id(level_id),
 	key goods_id(goods_id)
-)engine=InnoDB default charset=utf8 comment '会员价格';
+)engine=MyISAM default charset=utf8 comment '会员价格';
 
 drop table if exists shop_goods_pic;
 create table shop_goods_pic
@@ -67,7 +67,55 @@ create table shop_goods_pic
 	goods_id mediumint unsigned not null comment '商品Id',
 	primary key (id),
 	key goods_id(goods_id)
-)engine=InnoDB default charset=utf8 comment '商品相册';
+)engine=MyISAM default charset=utf8 comment '商品相册';
+
+
+/****属性相关表*****/
+DROP TABLE IF EXISTS shop_type;
+create table shop_type
+(
+	id mediumint unsigned not null auto_increment comment 'Id',
+	type_name VARCHAR(30) not null comment '类型名称',
+	primary key (id)
+)engine=MyISAM default charset=utf8 comment '类型';
+
+
+DROP TABLE IF EXISTS shop_attribute;
+create table shop_attribute
+(
+	id mediumint unsigned not null auto_increment comment 'Id',
+	attr_name VARCHAR(30) not null comment '属性名称',
+	attr_type enum('唯一','可选') not null comment '属性类型',
+	attr_option_values VARCHAR(300) not null DEFAULT'' comment '属性可选值',
+	type_id mediumint unsigned not NULL comment '所属类型Id',
+	primary key (id),
+	key type_id(type_id)
+)engine=MyISAM default charset=utf8 comment '属性';
+
+
+DROP TABLE IF EXISTS shop_goods_attr;
+create table shop_goods_attr
+(
+	id mediumint unsigned not null auto_increment comment 'Id',
+	attr_value VARCHAR(150) not null DEFAULT '' comment '属性值',
+	attr_id mediumint unsigned not null comment '属性Id',
+	goods_id mediumint unsigned not null comment '商品Id',
+	primary key (id),
+	key goods_id(goods_id),
+	key attr_id(attr_id)
+)engine=MyISAM default charset=utf8 comment '商品属性';
+
+
+DROP TABLE IF EXISTS shop_goods_number;
+create table shop_goods_number
+(
+	goods_id mediumint unsigned not null comment '商品Id',
+	goods_number mediumint unsigned not null DEFAULT'0' comment '库存量',
+	goods_attr_id varchar(150) not null comment '商品属性表ID，如果有多个，就用程序拼成字符串存到这个字段中',
+	key goods_id(goods_id)
+)engine=MyISAM default charset=utf8 comment '库存量';
+
+
 
 
 
